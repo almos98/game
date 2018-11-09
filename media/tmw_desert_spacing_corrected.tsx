b32904752b106abc559b64a49d2842ae8e5a662b
Alexion Ramos <ramosa@tcd.ie>
2018-11-09 22:46:11 +0000

    Added Tiled files.

---
+++ media/tmw_desert_spacing_corrected.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tmw_desert_spacing_corrected" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing_corrected.png" width="256" height="192"/>
+ <tile id="25">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="13.5839" width="32.0404" height="9.1544"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="1" x="12.698" y="-0.147652" width="9.30205" height="32.0404"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index">
+   <object id="4" x="14.1746" y="-0.147652" width="8.41614" height="31.8927"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index">
+   <object id="2" x="13.776" y="14.448">
+    <polygon points="0,0 18.1439,-0.167999 18.4799,7.05598 8.39997,7.05598 8.39997,16.9679 0,17.1359"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index">
+   <object id="2" x="0.176256" y="13.4836">
+    <polygon points="0,0 21.5914,-0.176256 21.7677,18.5951 12.7786,18.5951 12.7786,8.37218 0,8.46031"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.147652" y="14.1746" width="32.0404" height="7.67788"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <objectgroup draworder="index">
+   <object id="1" x="14.112" y="0.335999">
+    <polygon points="0,0 7.72798,-0.167999 7.55998,11.76 17.9759,12.096 17.6399,21.3359 -0.503998,20.8319"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="13.104">
+    <polygon points="0,0 13.44,0.167999 13.272,-13.104 21.8399,-12.936 22.0079,8.73597 0.167999,8.73597"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
